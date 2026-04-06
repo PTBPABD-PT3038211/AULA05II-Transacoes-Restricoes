a1e9644b93e9baa73dbdcb67367ffe52ee2c6c61
joshmerces <joshua.m@aluno.ifsp.edu.br>
2026-04-06 20:45:47 -0300

    Update aaa.sql

--- aaa.sql
+++ aaa.sql
@@ -1,17 +1,61 @@
+-- Questão 1
+
+CREATE SCHEMA aula05-transacoes-restricoes
+
 -- Questão 2
 
 CREATE TABLE company
 (
-    company_name PRIMARY KEY NOT NULL,
-    city
+    company_name VARCHAR(50) PRIMARY KEY NOT NULL,
+    city VARCHAR(50)
 );
 
 -- Questão 3
 
 CREATE TABLE employee
 (
-    person_name PRIMARY KEY NOT NULL,
-    street,
-    city
+    person_name VARCHAR(50) PRIMARY KEY NOT NULL,
+    street VARCHAR(50),
+    city VARCHAR(50)
 );
 
+-- Questão 4
+
+CREATE TABLE manages
+(
+    person_name VARCHAR(50) PRIMARY KEY NOT NULL,
+    manager_name VARCHAR(50)
+);
+
+-- Questão 5
+
+CREATE TABLE works
+(
+    person_name VARCHAR(50) PRIMARY KEY NOT NULL,
+    company_name VARCHAR(50) NOT NULL,
+    salary DECIMAL(10, 2)
+);
+
+-- Questão 6
+
+ALTER TABLE works
+ADD CONSTRAINT fk_works_employee
+FOREIGN KEY (person_name) REFERENCES employee(person_name)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- Questão 7
+
+ALTER TABLE works
+ADD CONSTRAINT fk_works_company
+FOREIGN KEY (company_name) REFERENCES company(company_name)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- Questão 8
+
+ALTER TABLE manages
+ADD CONSTRAINT fk_manages_employee
+FOREIGN KEY (person_name) REFERENCES employee(person_name)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
