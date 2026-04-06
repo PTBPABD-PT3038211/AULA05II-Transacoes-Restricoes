cbd98ca461dc39787e275974c9ee645fa7d541f4
joshmerces <joshua.m@aluno.ifsp.edu.br>
2026-04-06 20:40:46 -0300

    Add files via upload

---
+++ aaa.sql
@@ -0,0 +1,17 @@
+-- Questão 2
+
+CREATE TABLE company
+(
+    company_name PRIMARY KEY NOT NULL,
+    city
+);
+
+-- Questão 3
+
+CREATE TABLE employee
+(
+    person_name PRIMARY KEY NOT NULL,
+    street,
+    city
+);
+
